0ffd379addc372581665caa31e616db019c0198c
DavidJurich <davjur@gmail.com>
2021-12-13 16:58:43 -0500

    Assignment submission
    Functional carousel with keyboard arrow commands.

--- js/app.js
+++ js/app.js
@@ -1,44 +1,58 @@
 window.onload = () => {
-let current = 1
+    let current = 1;
 
-// const leftarrow = document.getElementById(`left-arrow`);
-// const rightarrow = document.getElementById(`right-arrow`);
+    // const leftarrow = document.getElementById(`left-arrow`);
+    // const rightarrow = document.getElementById(`right-arrow`);
 
-document.addEventListener('keydown', function(e) {
-    console.log(e);
-    if (e.key === `ArrowRight`) {
-    next();
-    } else if (e.key === `ArrowLeft`) {
-    last();
+    document.addEventListener(`keydown`, function(e) {
+        console.log(e);
+        if (e.key === `ArrowRight`) {
+            next();
+        } else if (e.key === `ArrowLeft`) {
+            last();
+        }
+    });
+
+    // let checkSlide = () => {
+    //     if (index > 0 && index < 4) {
+    //         leftarrow.classList.remove(`hidden`);
+    //         rightarrow.classList.remove(`hidden`);
+    //     } else if (index === 0) {
+    //         leftarrow.classList.add(`hidden`);
+    //     } else if (index === 4){
+    //         rightarrow.classList.add(`hidden`);
+    //     }
+    // };
+
+
+    function next() {
+        if (current === 4)
+        {
+            current = 1;
+
+            window.location.href = `#slide-${current}`;
+            // `#slide-${current}`.classlist.add(`.move`);
+        } else {
+            current += 1;
+            window.location.href = `#slide-${current}`;
+            // current.classlist.add(`.move`);
+            // checkSlide();
+        }
     }
-});
 
+    function last() {
 
-function next() {
-    if (current === 4)
-    {
-        current = 1
+        if (current === 1) {
+            current = 4;
 
-        window.location.href = `#slide-${current}`
-        // `#slide-${current}`.classlist.add(`.move`);
-    } else {
-        current += 1
-        window.location.href = `#slide-${current}`
-        // current.classlist.add(`.move`);
+            window.location.href = `#slide-${current}`;
+            // `#slide-${current}`.classlist.add(`.move`);
+        } else {
+            current -= 1;
+            window.location.href = `#slide-${current}`;
+            // `#slide-${current}`.classlist.add(`.move`);
+            // checkSlide();
+
+        }
     }
-}
-
-function last() {
-
-    if (current === 1) {
-        current = 4
-
-        window.location.href = `#slide-${current}`
-        // `#slide-${current}`.classlist.add(`.move`);
-    } else {
-        current -= 1
-        window.location.href = `#slide-${current}`
-        // `#slide-${current}`.classlist.add(`.move`);
-    }
-}
 };
