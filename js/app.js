7c9a44e21fe51f3c1427a34e4f3ac41258eb4d14
DavidJurich <davjur@gmail.com>
2021-12-13 17:40:18 -0500

    Final cleanup
    Rough cleanups of ideas/comments.

--- js/app.js
+++ js/app.js
@@ -1,9 +1,6 @@
 window.onload = () => {
     let current = 1;
 
-    // const leftarrow = document.getElementById(`left-arrow`);
-    // const rightarrow = document.getElementById(`right-arrow`);
-
     document.addEventListener(`keydown`, function(e) {
         console.log(e);
         if (e.key === `ArrowRight`) {
@@ -13,46 +10,24 @@ window.onload = () => {
         }
     });
 
-    // let checkSlide = () => {
-    //     if (index > 0 && index < 4) {
-    //         leftarrow.classList.remove(`hidden`);
-    //         rightarrow.classList.remove(`hidden`);
-    //     } else if (index === 0) {
-    //         leftarrow.classList.add(`hidden`);
-    //     } else if (index === 4){
-    //         rightarrow.classList.add(`hidden`);
-    //     }
-    // };
-
-
     function next() {
         if (current === 4)
         {
             current = 1;
-
             window.location.href = `#slide-${current}`;
-            // `#slide-${current}`.classlist.add(`.move`);
         } else {
             current += 1;
             window.location.href = `#slide-${current}`;
-            // current.classlist.add(`.move`);
-            // checkSlide();
         }
     }
 
     function last() {
-
         if (current === 1) {
             current = 4;
-
             window.location.href = `#slide-${current}`;
-            // `#slide-${current}`.classlist.add(`.move`);
         } else {
             current -= 1;
             window.location.href = `#slide-${current}`;
-            // `#slide-${current}`.classlist.add(`.move`);
-            // checkSlide();
-
         }
     }
 };
