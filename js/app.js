83418f757edffa15d1fae4f0d36b87328e13c3fd
DavidJurich <davjur@gmail.com>
2021-12-13 15:07:27 -0500

    Update all files
    Organize content, add gulp scaffolding, establish necessary steps for progress.

--- js/app.js
+++ js/app.js
@@ -1,5 +1,8 @@
+window.onload = () => {
 let current = 1
 
+// const leftarrow = document.getElementById(`left-arrow`);
+// const rightarrow = document.getElementById(`right-arrow`);
 
 document.addEventListener('keydown', function(e) {
     console.log(e);
@@ -12,27 +15,30 @@ document.addEventListener('keydown', function(e) {
 
 
 function next() {
-
-    if (current === 3) {
+    if (current === 4)
+    {
         current = 1
 
         window.location.href = `#slide-${current}`
+        // `#slide-${current}`.classlist.add(`.move`);
     } else {
         current += 1
         window.location.href = `#slide-${current}`
-
+        // current.classlist.add(`.move`);
     }
 }
 
 function last() {
 
     if (current === 1) {
-        current = 3
+        current = 4
 
         window.location.href = `#slide-${current}`
+        // `#slide-${current}`.classlist.add(`.move`);
     } else {
         current -= 1
         window.location.href = `#slide-${current}`
-
+        // `#slide-${current}`.classlist.add(`.move`);
     }
 }
+};
